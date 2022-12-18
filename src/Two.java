f17ea5cd63740478a4cfbf582a1efbaafea5b0cd
Pavel Khudenko <khudenk0pave1@gmail.com>
2022-12-18 15:06:22 +0300

    Calculator

--- src/Two.java
+++ src/Two.java
@@ -3,5 +3,9 @@ import java.util.ArrayList;
 public class Two {
     public static void main(String[] args) {
         ArrayList<String> boom = new ArrayList<>();
+        int x = 0;
+        int a = 5;
+        int xa = x * a;
+        System.out.println(xa);
     }
 }
